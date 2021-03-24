fcf27374b4c60757f00df74797e373bf51422b64
Johnney Chen <mzchenca@gmail.com>
2021-03-24 09:29:19 -0600

    made home page

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import Navbar from './Navbar'
+import Main from './Main'
+
+const App = () => {
+    return (
+        <React.Fragment>
+            <Navbar></Navbar>
+            <Main></Main>
+        </React.Fragment>
+    )
+}
+
+export default App
